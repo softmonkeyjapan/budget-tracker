91464cf6ccb657ffba0a38c6435fad3d0925864b
Loic KARTONO <kartono.loic@gmail.com>
2026-08-06 07:43:13 +1100

    chore: scaffold shadcn checkbox, label, dialog, dropdown-menu, input primitives

--- resources/js/Components/DangerButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="inline-flex items-center rounded-control bg-expense px-5 py-2.5 text-sm font-semibold text-white transition duration-150 ease-in-out hover:bg-expense/90 focus:outline-none focus:ring-2 focus:ring-expense focus:ring-offset-2 disabled:opacity-40"
-    >
-        <slot />
-    </button>
-</template>
--- resources/js/Components/PrimaryButton.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <button
-        class="inline-flex items-center rounded-control bg-nav px-5 py-2.5 text-sm font-semibold text-white shadow-primary transition duration-150 ease-in-out hover:bg-nav/90 focus:outline-none focus:ring-2 focus:ring-nav focus:ring-offset-2 disabled:opacity-40"
-    >
-        <slot />
-    </button>
-</template>
