5f636760ad37eb8c4479a9f2a2e7b04760504113
Loic KARTONO <kartono.loic@gmail.com>
2026-08-06 08:18:42 +1100

    chore: scaffold shadcn sheet, separator, skeleton, tooltip, sidebar primitives

---
+++ resources/js/Components/ui/sidebar/SidebarGroup.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  class: {
+    type: [Boolean, null, String, Object, Array],
+    required: false,
+    skipCheck: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    data-slot="sidebar-group"
+    data-sidebar="group"
+    :class="cn('relative flex w-full min-w-0 flex-col p-2', props.class)"
+  >
+    <slot />
+  </div>
+</template>
