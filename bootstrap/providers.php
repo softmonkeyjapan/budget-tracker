131e88f891c61c2100e5d55cb97621f471201913
Loic KARTONO <kartono.loic@gmail.com>
2026-08-11 11:31:11 +1100

    chore(domains): decommission RepositoryServiceProvider (#71)

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,11 +6,9 @@ use App\Domains\Feedback\Providers\FeedbackServiceProvider;
 use App\Domains\Incomes\Providers\IncomesServiceProvider;
 use App\Domains\Users\Providers\UsersServiceProvider;
 use App\Providers\AppServiceProvider;
-use App\Providers\RepositoryServiceProvider;
 
 return [
     AppServiceProvider::class,
-    RepositoryServiceProvider::class,
     UsersServiceProvider::class,
     IncomesServiceProvider::class,
     CategoriesServiceProvider::class,
